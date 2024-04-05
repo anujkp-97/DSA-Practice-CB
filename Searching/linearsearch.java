1091ac5b402309167c617551c1a42b1cb400fb58
anujkp-97 <anujpal9770@gmail.com>
2024-04-05 23:06:14 +0530

    changes in linear search

--- Searching/linearsearch.java
+++ Searching/linearsearch.java
@@ -3,6 +3,7 @@ public class linearsearch {
     public static void main(String[] args) {
         
         System.out.println("Hello this is the code from cloning the git repository.");
+        System.out.println("what is the system log on the call");
     }
 
     
