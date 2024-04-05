dbfe186d9316ac940daeb3ec29b278dd89c3a5ae
anujkp-97 <anujpal9770@gmail.com>
2024-04-05 23:04:04 +0530

    linearsearch

---
+++ Searching/linearsearch.java
@@ -0,0 +1,9 @@
+package Searching;
+public class linearsearch {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello this is the code from cloning the git repository.");
+    }
+
+    
+}
