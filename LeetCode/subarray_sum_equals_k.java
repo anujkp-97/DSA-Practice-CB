0282e4c44372c902515ebe8750c270367a1bec3e
anujkp-97 <anujpal9770@gmail.com>
2024-05-01 17:48:39 +0530

    leetcode

--- LeetCode/subarray_sum_equals_k.java
+++ LeetCode/subarray_sum_equals_k.java
@@ -1,28 +1,30 @@
 package LeetCode;
 
 public class subarray_sum_equals_k {
-    public static void sum(int arr[], int k){
-            int count =0;
-            int element = 0;
+    public static int sum(int arr[], int k){
+        int n = arr.length; // size of the given array.
+        int cnt = 0; // Number of subarrays:
 
-            for(int i=0; i<arr.length; i++)
-            {
-                element = arr[i];
-                for(int j=i; j<arr.length; j++)
-                {
-                    if(element == k){
-                        count++;
-                    }
-                    element+= arr[j];
-                }
+        for (int i = 0 ; i < n; i++) { // starting index i
+            int sum = 0;
+            for (int j = i; j < n; j++) { // ending index j
+                // calculate the sum of subarray [i...j]
+                // sum of [i..j-1] + arr[j]
+                sum += arr[j];
+
+                // Increase the count if sum == k:
+                if (sum == k)
+                    cnt++;
             }
-            System.out.println(count);
+        }
+        return cnt;
     }
     public static void main(String[] args) {
         
-        int arr[]={1,2,3};
-
-        sum(arr,3);
+        int arr[]={1,1,1};
+ 
+        int res = sum(arr,2);
+        System.out.println(res);
     }
     
 }
