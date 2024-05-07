63c0413603e0596a1ca7beb803388a368afc4d34
anujkp-97 <anujpal9770@gmail.com>
2024-05-07 16:58:57 +0530

    Linked list

--- LinkedList/add_Element.java
+++ LinkedList/add_Element.java
@@ -2,90 +2,155 @@ package LinkedList;
 
 public class add_Element {
 
-    class Node{
-         int val;
-         Node next;
-    }
-        private int size;
-        private Node head;
-        private Node tail;
+    class Node {
+		int val;
+		Node next;
 
-        public void addFirst(int item)
-        {
-            Node nn= new Node();
-            nn.val = item;
+	}
 
-            if(size ==0){
-                head =nn;
-                tail =nn;
-                size++;
-            }else{
-                nn.next = head;
-                head = nn;
-            }
-        }
-        // add last
+	private Node head;
+	private Node tail;
+	private int size;
 
-        public void addLast(int item)
-        {
-            if(size ==0){
-                addFirst(item);
-            }else{
-                 Node nn = new Node();
-                nn.val = item;
-                tail.next = nn;
-                tail = nn;
-                size++;
+	// O(1)
+	public void AddFirst(int item) {
+		Node nn = new Node();
+		nn.val = item;
+		if (size == 0) {
+			head = nn;
+			tail = nn;
+			size++;
+		} else {
+			nn.next = head;
+			head = nn;
+			size++;
 
-            }
+		}
 
-        }
+	}
 
-        public void addIndex(int k, int item) throws Exception
-        {
-            if(k ==0){
-                addFirst(item);
-            }
-            else if(k ==size){
-                addLast(item);
-            }
-            else{
-                Node nn = new Node();
-                nn.val = item;
-                Node prev = getNode(k-1);
-                nn.next = prev.next;
-                prev.next = nn;
-                size++;
-            }
-        }
+	// O(1)
+	public void AddLast(int item) {
+		if (size == 0) {
+			AddFirst(item);
+		} else {
+			Node nn = new Node();
+			nn.val = item;
+			tail.next = nn;
+			tail = nn;
+			size++;
+		}
 
-        // access the address of particular node
-        public Node getNode(int k) throws Exception{
-            if(k<0 || k>=size)
-            {
-                throw new Exception("Baklol size dekh eska!!");
-            }
-            Node tmp  = head;
-            for(int i=0; i<k; i++)
-            {
-                tmp = tmp.next;
+	}
 
-            }
-            return tmp;
-        }
+	// O(N)
+	public void AddIndex(int k, int item) throws Exception {
+		if (k == 0) {
+			AddFirst(item);
+		} else if (k == size) {
+			AddLast(item);
+		} else {
+			Node nn = new Node();
+			nn.val = item;
+			Node prev = getNode(k - 1);
+			nn.next = prev.next;
+			prev.next = nn;
+			size++;
 
-        // display
-        public void display(){
-            Node tmp;
-            tmp = head;
+		}
+	}
 
-            while(tmp !=null)
-            {
-                System.out.print(tmp.val+"-->");
-                tmp = tmp.next;
-            }
-            System.out.println(".");
-        }
+	// O(1)
+	public int getfirst() {
+		return head.val;
+	}
+
+	// O(1)
+	public int getlast() {
+		return tail.val;
+	}
+
+	// O(N)
+	public int getindex(int k) throws Exception {
+		return getNode(k).val;
+	}
+
+	// O(1)
+	public int removefirst() {
+		int val = head.val;
+		if (size == 1) {
+			head = null;
+			tail = null;
+			size--;
+		} else {
+			Node temp = head;
+			head = head.next;
+			temp.next = null;
+			size--;
+		}
+
+		return val;
+
+	}
+
+	// O(N)
+	public int removeLast() throws Exception {
+		if (size == 1) {
+			return removefirst();
+		} else {
+			Node nn = getNode(size - 2);
+			int val = tail.val;
+			tail = nn;
+			tail.next = null;
+			size--;
+			return val;
+		}
+	}
+
+	// O(N)
+	public int removeindex(int k) throws Exception {
+		if (k == 0) {
+			return removefirst();
+		} else if (k == size - 1) {
+			return removeLast();
+		} else {
+			Node prev = getNode(k - 1);
+			Node curr = prev.next;
+			prev.next = curr.next;
+			curr.next = null;
+			size--;
+			return curr.val;
+		}
+
+	}
+
+	public int size() {
+		return size;
+	}
+
+	private Node getNode(int k) throws Exception {
+
+		if (k < 0 || k >= size) {
+			throw new Exception("Bklol k range me to dedo");
+		}
+		Node temp = head;
+		for (int i = 0; i < k; i++) {
+			temp = temp.next;
+		}
+		return temp;
+	}
+
+	public void display() {
+		Node temp = head;
+		while (temp != null) {
+			System.out.print(temp.val + "-->");
+			temp = temp.next;
+
+		}
+		System.out.println(".");
+
+	}
+
 
     }
     
--- LinkedList/add_client.java
+++ LinkedList/add_client.java
@@ -4,18 +4,22 @@ public class add_client {
     public static void main(String[] args) throws Exception {
         
         add_Element ll = new add_Element();
-        ll.addFirst(10);
-        ll.addFirst(20);
-        ll.addFirst(30);
-
-        ll.display();
-        ll.addLast(5);
-        ll.display();
-
-        ll.addFirst(2);
-        ll.display();
-        ll.addIndex(3, 3);
-        ll.display();
+        ll.AddFirst(10);
+		ll.AddFirst(20);
+		ll.AddFirst(30);
+		ll.AddLast(4);
+		ll.AddLast(5);
+		ll.display();
+		ll.AddIndex(3, 8);// index , element
+		ll.display();
+		//System.out.println(ll.size());
+		System.out.println(ll.getfirst());
+		System.out.println(ll.getlast());
+		System.out.println(ll.getindex(2));
+		//System.out.println(ll.removefirst());
+		//System.out.println(ll.removeLast());
+		System.out.println(ll.removeindex(2));
+		ll.display();
     }
     
 }
